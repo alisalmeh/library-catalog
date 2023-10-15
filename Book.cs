5b6e052ddec6f66332b795616ea1be80d542ae5c
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-15 14:00:43 +0330

    add Book class

---
+++ Book.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliSalmeh_ProjectWeek8_LibraryCatalog
+{
+    abstract public class Book
+    {
+        public abstract override string ToString();
+    }
+}
