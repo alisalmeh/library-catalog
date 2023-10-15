c8e9747c677e66a26d524a5e450ac120b91acad9
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-15 14:02:09 +0330

    add Program class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace AliSalmeh_ProjectWeek8_LibraryCatalog
     {
         static void Main(string[] args)
         {
-            
+            // Create an array of books with different types
+            Book[] libraryCatalog = new Book[]
+            {
+                new FictionBook("The Great Gatsby", "F. Scott Fitzgerald", 180, "Classic"),
+                new FictionBook("Dune", "Frank Herbert", 412, "Science Fiction"),
+                new NonFictionBook("Sapiens", "Yuval Noah Harari", 512, "History"),
+                new NonFictionBook("Atomic Habits", "James Clear", 320, "Self-Help"),
+            };
+
+            // Display information about all the books in the library
+            Library.DisplayBooks(libraryCatalog);
         }
     }
 }
