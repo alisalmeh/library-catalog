2a9c2f918746cc09a5765a50822aead03393909d
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-15 14:01:31 +0330

    add Library class

---
+++ Library.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliSalmeh_ProjectWeek8_LibraryCatalog
+{
+    public class Library
+    {
+        public static void DisplayBooks(Book[] books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
